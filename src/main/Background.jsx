7b91d098937dd34e9291acdf5ae222984829ff6b
aliengitman <gitman-1@aliendc.com>
2025-04-02 02:55:20 +0700

    feat: Implement ground image animation in Background component

--- src/main/Background.jsx
+++ src/main/Background.jsx
@@ -1,10 +1,31 @@
+import { useEffect, useState } from 'react';
 import bg from '../assets/image/bg.jpg';
+import ground from '../assets/image/ground.jpg';
 
 const Background = () => {
+    const [groundImageIndex, setGroundImageIndex] = useState(0);
+    const gSpeed = 4;
+
+    useEffect(()=> {
+        const interval = setInterval(() => {
+            setGroundImageIndex((prevPov) => prevPov - gSpeed);
+            if (groundImageIndex <= -window.innerWidth) {
+                setGroundImageIndex(0);
+            }
+        }, 900 / 60);
+
+        return () => clearInterval(interval);
+    }, [groundImageIndex]);
     return (
         <>
             <div className="fixed inset-0 z-0 flex items-center justify-center">
-                    <img src={bg} alt="background" className='drop-shadow-lg'/>
+                <img src={bg} alt="background" className="object-cover"/>
+                    <img src={ground} className='absolute w-screen h-4/8 pt-12 bottom-0.5'
+                    style={{ left: `${groundImageIndex}px` }}
+                    alt='rumput'/>
+                    <img src={ground} className='absolute w-screen h-4/8 pt-12 bottom-0.5 '
+                    style={{ left: `${groundImageIndex + window.innerWidth}px` }}
+                    alt='rumput'/>
             </div>
         </>
     );
