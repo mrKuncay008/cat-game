56e8fd82cc7ce3c8d42781d3e30550fba864ca21
mrKuncay008 <kuncaymuhammad@gmail.com>
2025-04-02 20:10:00 +0700

    feat: add glitch animation to TitleDemo component and update styling

--- src/main/Title.jsx
+++ src/main/Title.jsx
@@ -1,8 +1,10 @@
 const TitleDemo = () => {
     return (
         <>
-            <div className="fixed top-4 lg:top-24 flex items-start justify-start text-pixel-sans">
-                <h1>Coming Soon</h1>
+            <div className="fixed  top-4 lg:top-20 flex items-start
+            justify-start text-pixel-sans h-32">
+                <h1 className="mt-8 glitch font-medium">Coming Soon</h1>
+                <span>By Maspren</span>
             </div>
         </>
     )
