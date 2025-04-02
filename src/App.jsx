dfb570c9a013fb0c45e52538aee19e351b8de606
mrKuncay008 <kuncaymuhammad@gmail.com>
2025-04-02 18:48:16 +0700

    feat: add screen orientation detection and prompt for rotation

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,34 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import Cat from './main/Cat'
 import Background from './main/Background'
+import { use } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isPortrait, setIsPortrait] = useState(false);
+
+  useEffect(() => {
+    const checktOriant = () => {
+      setIsPortrait(window.innerHeight > window.innerWidth);
+    };
+
+    window.addEventListener('resize', checktOriant);
+    checktOriant();
+
+    return () => {
+      window.removeEventListener('resize', checktOriant);
+    };
+  }, []);
 
   return (
     <>
-      <Background />
-      <Cat />
+      {isPortrait && (
+        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-red-500 text-white p-4 rounded-md shadow-md">
+        <p>Please Roatate your screen !</p>
+      </div>
+      )}
+      <Background isPortrait={isPortrait}/>
+      <Cat isPortrait={isPortrait}/>
     </>
   )
 }
