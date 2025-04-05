1f106d1d57aa2ad1d81689dbc9580dd354654b9c
mrKuncay008 <kuncaymuhammad@gmail.com>
2025-04-05 22:25:06 +0700

    fix: interval bg

--- src/main/Background.jsx
+++ src/main/Background.jsx
@@ -4,7 +4,10 @@ import ground from '../assets/image/ground.png';
 
 const Background = () => {
     const [groundImageIndex, setGroundImageIndex] = useState(0);
-    const gSpeed = 4;
+    const gSpeed = 4.5;
+
+    const [bgImageIndex, setBgImageIndex] = useState(0);
+    const bSpeed = 1.5;
 
     useEffect(()=> {
         const interval = setInterval(() => {
@@ -12,22 +15,34 @@ const Background = () => {
             if (groundImageIndex <= -window.innerWidth) {
                 setGroundImageIndex(0);
             }
-        }, 900 / 70);
+        }, 600 / 60);
 
         return () => clearInterval(interval);
     }, [groundImageIndex]);
+
+    useEffect(()=> {
+        const intervalBg = setInterval(() => {
+            setBgImageIndex((prevPov) => prevPov - bSpeed);
+            if (bgImageIndex <= -window.innerWidth) {
+                setBgImageIndex(0);
+            }
+        }, 600 / 60);
+
+        return () => clearInterval(intervalBg);
+    }, [groundImageIndex]);
+
     return (
         <>
             <div className="fixed w-screen flex items-center justify-center">
                 <img src={bg} 
                 className='fixed w-screen'
                 alt="background"
-                style={{ left: `${groundImageIndex}px` }}
+                style={{ left: `${bgImageIndex}px` }}
                 /> 
                 <img src={bg} 
                 className='fixed w-screen'
                 alt="background"
-                style={{ left: `${groundImageIndex + window.innerWidth}px` }}
+                style={{ left: `${bgImageIndex + window.innerWidth}px` }}
                 />
                     <img src={ground} className='fixed w-screen h-4/8 pt-2 lg:pt-6 bottom-0.5'
                     style={{ left: `${groundImageIndex}px` }}
